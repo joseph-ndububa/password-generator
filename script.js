6cb54def60485f5ed65fc868bea662229585575c
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-05-09 03:44:35 -0400

    added objects for characters

--- script.js
+++ script.js
@@ -1,70 +1,84 @@
 // arrays of all characters
+const number = {
+  chars: "0123456789"
+  confirm: ""
+}
 
-const numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const uppercase = {
+  chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  confirm: ""
+}
 
-const uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = {
+  chars: "abcdefghijklmnopqrstuvwxyz";
+  confirm: ""
+}
 
-const lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const specials = {
+  chars: "!#$%&*+-./:;<=>?@ ^_~";
+  confirm: ""
+}
 
-const specialChars = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "_", "~"];
 // Prompt functions
 
 function lengthPrompt() {
-  let pwLength = prompt("Please choose a password length between 8 and 128 characters")
+  const pwLength = prompt("Please choose a password length between 8 and 128 characters")
 }
 
 function lowercasePrompt() {
-  let lower = prompt("Include lower case characters? Input Y for yes, N for no.")
+  const lower = prompt("Include lower case characters? Input Y for yes, N for no.")
 }
 
 function uppercasePrompt() {
-  let upper = prompt("Include lower case characters? Input Y for yes, N for no.")
+  const upper = prompt("Include lower case characters? Input Y for yes, N for no.")
 }
 
 function numericPrompt() {
-  let numeric = prompt("Include numeric characters? Input Y for yes, N for no.")
+  const numeric = prompt("Include numeric characters? Input Y for yes, N for no.")
 }
 
 function specialPrompt() {
-  let special = prompt("Include special characters? Input Y for yes, N for no.")
+  const special = prompt("Include special characters? Input Y for yes, N for no.")
 }
 
 function promptSequence() {
-  lengthPrompt()
-  if (typeof pwLength === "number" && pwLength >= 8 && pwLength <= 128) {
-    lowercasePrompt();
-  }
-  else {
-    lengthPrompt();
+
+  lengthPrompt();
+
+  uppercasePrompt();
+
+  lowercasePrompt();
+
+  numericPrompt();
+
+  uppercasePrompt();
+
+  specialPrompt();
+
+  numericPrompt();
+
+  generatePassword()
+
+}
+
+// password generation function
+
+function generate() {
+
+  if (lower === "Y" && upper === "Y" && numeric === "Y" && special === "Y") {
+    const selection = string.concat(numberChars, uppercaseChars, lowercaseChars, specialChars)
   }
 
-  if (lower === "Y" || lower === "y" || lower === "N" || lower === "n") {
-    uppercasePrompt();
-  }
-  else {
-    lowercasePrompt();
+  else if (lower === "Y" && upper === "Y" && numeric === "Y" && special === "N") {
+    const selection = string.concat(numberChars, uppercaseChars, lowercaseChars, specialChars)
   }
 
-  if (upper === "Y" || upper === "y" || upper === "N" || upper === "n") {
-    numericPrompt();
-  }
-  else {
-    uppercasePrompt();
+
+
+  for (i = 0; i < pwLength; i++) {
+
   }
 
-  if (numeric === "Y" || numeric === "y" || numeric === "N" || numeric === "n") {
-    specialPrompt();
-  }
-  else {
-    numericPrompt();
-  }
-
-  if (special === "Y" || special === "y" || special === "N" || special === "n") {
-    generatePassword();
-  }
-  else {
-    specialPrompt();
-  }
 }
 
 // Get references to the #generate element
@@ -80,4 +94,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", promptSequence);
